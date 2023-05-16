198971244f165b4ed8dd2e23207ca896f0f1690d
MATHEUS MORAES PIOVESAN <mmp22@inf.ufpr.br>
2023-05-16 16:59:31 -0300

    [ADD] func enque

--- tp4/libfila.c
+++ tp4/libfila.c
@@ -1,28 +1,17 @@
 #include "libfila.h"
 #include <stdlib.h>
 
-// typedef struct nodo {
-//     int dado;
-//     struct nodo *prox;
-// } nodo_t;
-
-// typedef struct fila {
-//     nodo_t *cabeca;
-//     nodo_t *cauda;
-//     int tamanho;
-// } fila_t;
-
 /* 
  * Cria e retorna uma nova fila.
  * Retorna NULL em caso de erro de alocação.
 */
 fila_t *fila_cria () {
     struct fila *f1;
-    f1 = malloc (sizeof(struct fila));
+    f1 = malloc (sizeof(fila_t));
 
     if (f1 = NULL)
         return NULL;
-        
+
     f1->cabeca = NULL;
     f1->cauda = NULL;
     f1->tamanho = 0;
@@ -42,11 +31,14 @@ void fila_destroi (fila_t **fila) {
 int enqueue (fila_t *fila, int dado) {
     nodo_t *novoDado;
     novoDado = malloc(sizeof(nodo_t));
-    
+
     novoDado->dado = dado;
     novoDado->prox = NULL;
 
-    fila->cauda = &novoDado;
+    if(fila_vazia(fila))
+        fila->cabeca = novoDado;
+
+    fila->cauda->prox = novoDado;
     fila->tamanho++;
 }
 
@@ -68,7 +60,7 @@ int fila_tamanho (fila_t *fila) {
 
 /* Retorna 1 se fila vazia, 0 em caso contrario. */ 
 int fila_vazia (fila_t *fila) {
-    if ((fila->cabeca != NULL) && (fila->cauda != NULL))
-        return 0;
-    return 1;
+    if (fila_tamanho == 0)
+        return 1;
+    return 0;
 }
