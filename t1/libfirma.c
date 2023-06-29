de26212502610f3dce9c1466cc6792ee78639aa5
Matheus <matheusmoraespiovesan@gmail.com>
2023-06-28 21:44:08 -0300

    firmando as parada da firma

--- t1/libfirma.c
+++ t1/libfirma.c
@@ -66,34 +66,62 @@ int main() {
 
     funcionario_t *funcionarios = inicializa_funcionarios();
     tarefa_t *tarefas = inicializa_tarefas();
-    compromisso_t *novoCompr = cria_compromisso(gera_horario(), gera_id, "Reuniao");
 
-    int aleat;
+    /* ESCOLHER FUNCIONÁRIO ALEATÓRIO DE 1 A 100 */
+    int aleat = rand() % 31;
 
     if (funcionarios == NULL || tarefas == NULL)
         return 1;
 
     for (int i = 0; i < 100; i++) {
-        aleat = rand() % 101;
 
-        if ((funcionarios[aleat].lideranca > 29) && (funcionarios[aleat].lideranca < 71)) {
-            int diaReuniao = rand() % (31) + 1;
-            if (marca_compromisso_agenda(funcionarios[aleat].agenda, diaReuniao, novoCompr)) {
-                int marcaDisponibilidade = 0;
+        /* ACHAR UM LÍDER (30 < valor.liderança < 70) */
+        while ((funcionarios[aleat].lideranca > 29) && (funcionarios[aleat].lideranca < 71))
+            aleat = rand() % 31;
 
-                for (int j = 0; j < (rand() % 5) + 2; j++) {
-                    int aleat2 = rand() % 31;
+        /* GERA DIA ALEATÓRIO E CRIA COMPROMISSO */
+        int diaReuniao = rand() % (31) + 1;
+        compromisso_t *novoCompr = cria_compromisso(gera_horario(), gera_id(), "Reuniao");
+        
+        hc_compr(novoCompr); // usar essa função pra pegar as horas de início e minutos e tals
 
-                    if ((funcionarios[aleat2].experiencia + (rand() % 31) - 20) < funcionarios[aleat].lideranca) {
-                        if (marca_compromisso_agenda(funcionarios[aleat2].agenda, diaReuniao, novoCompr /* ver isso aqui hein, não sei que compromisso tem que passar nesse parâmetro */)) {
-                            marcaDisponibilidade++;
-                        }
-                    }
-                }   
+        "REUNIR L %.2d %.2d/%.2d %.2d:%.2d %.2d:%.2d T %.2d", aleat, i, /* mes_atual */,  
+
+        - os valores são o LIDER, DIA, mes_atual, ini_h, ini_m, fim_h, fim_m, tarefa 
+
+        /* SE A AGENDA DO LÍDER ESTIVER DISPONÍVEL, MARCA E PROCURA 2-6 FUNCIONÁRIOS PRA MESMA TAREFA */
+        if (marca_compromisso_agenda(funcionarios[aleat].agenda, diaReuniao, novoCompr)) {
+            int marcaDisponibilidade = 0;
+            
+            for (int j = 0; j < (rand() % 5) + 2; j++) {
+                int aleat2 = rand() % 31;
+                if ((funcionarios[aleat2].experiencia + (rand() % 31) - 20) < funcionarios[aleat].lideranca) {
+                    if (marca_compromisso_agenda(funcionarios[aleat2].agenda, diaReuniao, novoCompr /* ver isso aqui hein, não sei que compromisso tem que passar nesse parâmetro */))
+                        marcaDisponibilidade++;   
+                }
             }
-        }
+            
+            /* SE NENHUM FUNCIONÁRIO PUDER PARTICIPAR, DESMARCA A REUNIÃO */
+            if(!marcaDisponibilidade)
+                desmarca_compromisso_agenda(funcionarios[aleat].agenda, diaReuniao, novoCompr);
+            else 
+
+
+        } else 
+            printf("\tLIDER INDISPONIVEL \n");
+
+        // prox_mes_agenda(); qual agenda passar no parâmetro?
     }
 
+    prim_mes_agenda(/* sei lá zé */);
+
+    for (int i = 1; i < 32; i++) {
+        int t = 0;
+
+        if (tarefas[t].tempo_conclusao > 0) {
+            tarefas[t].tempo_conclusao -= min_trab * (funcionarios[/* não sei qual */].experiencia / 100.0) * ((100 - tarefas[t].dificuldade) / 100.0);
+        }
+    }
 
     return 0;
 }
